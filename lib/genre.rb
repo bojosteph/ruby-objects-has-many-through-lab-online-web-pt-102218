878a9cf7445fe9829b19906739ecf736782c6f72
Roberto Samia <bojosteph@gmail.com>
2018-11-21 18:59:30 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,5 +2,16 @@ class Genre
   
   attr_accessor :name, :song, :genre,
   
+  @@all = []
+  
   def initialze(name)
     @name = name
+    @@all << name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  
+end
